a9652163684961bee4bd1954423af27797651a75
Michael Xu <1378606230@qq.com>
2022-10-19 06:53:36 +0800

    time: 100.00% space: 47.22% for leetcode 48

--- src/solution48.rs
+++ src/solution48.rs
@@ -1,8 +1,15 @@
 pub struct Solution;
 
 impl Solution {
-    pub fn template() {
-		
-	}
+    pub fn rotate(matrix: &mut Vec<Vec<i32>>) {
+        let mut tmp_matrix = matrix.clone();
+        let col = matrix[0].len() - 1;
+        for i in 0..matrix.len() {
+            for j in 0..matrix[0].len() {
+                tmp_matrix[j][col - i] = matrix[i][j];
+            }
+        }
+        *matrix = tmp_matrix.clone();
+    }
 }
 
