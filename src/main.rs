db700ca208c95fc3462fe80b78f1038a540bebb2
Michael Xu <1378606230@qq.com>
2022-10-19 14:28:26 +0800

    time: 100.00% space: 11.76% for leetcode 409

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod solution119;
 mod solution240;
 mod solution435;
 mod solution290;
+mod solution409;
 
 fn main() {
     
---
+++ src/solution409.rs
@@ -0,0 +1,27 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn longest_palindrome(s: String) -> i32 {
+        use std::collections::HashMap;
+        let mut current_hashmap: HashMap<char, i32> = HashMap::new();
+        let mut max_length = 0;
+        let mut flag = false;
+
+        for c in s.chars() {
+            current_hashmap.entry(c).and_modify(|x| *x += 1).or_insert(1);
+        }
+
+        for (key, val) in current_hashmap {
+            if val % 2 != 0 {
+                flag = true;
+            }
+            max_length += (val / 2) * 2;
+        }
+
+        if flag {
+            max_length + 1
+        } else {
+            max_length
+        }
+    }
+}
