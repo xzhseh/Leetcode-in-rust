1760c99f28e9f4a062e89a7296a9755206cd61ec
Michael Xu <1378606230@qq.com>
2022-10-19 15:20:50 +0800

    time: 100.00% space: 58.85% for leetcode 415

--- src/solution415.rs
+++ src/solution415.rs
@@ -6,13 +6,32 @@ impl Solution {
         let min_len = std::cmp::min(num1.len(), num2.len());
         let mut current_val = 0;
         let mut return_val = String::new();
+
         for i in 0..min_len {
-            let tmp1 = num1[i..(i + 1)].parse::<i32>().unwrap();
-            let tmp2 = num2[i..(i + 1)].parse::<i32>().unwrap();
-            return_val.push_str(&((tmp1 + tmp2) % 10).to_string());
+            let tmp1 = num1[(num1.len() - i - 1)..(num1.len() - i)].parse::<i32>().unwrap();
+            let tmp2 = num2[(num2.len() - i - 1)..(num2.len() - i)].parse::<i32>().unwrap();
+            return_val.push_str(&((tmp1 + tmp2 + current_val) % 10).to_string());
             current_val = (tmp1 + tmp2 + current_val) / 10;
         }
 
-        return_val
+        if num1.len() < num2.len() {
+            for i in min_len..num2.len() {
+                let tmp2 = num2[(num2.len() - i - 1)..(num2.len() - i)].parse::<i32>().unwrap();
+                return_val.push_str(&((tmp2 + current_val) % 10).to_string());
+                current_val = (tmp2 + current_val) / 10;
+            }
+        } else {
+            for i in min_len..num1.len() {
+                let tmp1 = num1[(num1.len() - i - 1)..(num1.len() - i)].parse::<i32>().unwrap();
+                return_val.push_str(&((tmp1 + current_val) % 10).to_string());
+                current_val = (tmp1 + current_val) / 10;
+            }
+        }
+
+        if current_val >= 1 {
+                return_val.push_str(&current_val.to_string());
+            }
+
+        return_val.chars().rev().collect::<String>()
     }
 }
