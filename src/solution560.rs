eab7b54198648c90efe89534ebb32161c8abbab2
Michael Xu <1378606230@qq.com>
2022-10-21 09:34:10 +0800

    time: 8.16% space: 95.92% for leetcode 560

--- src/solution560.rs
+++ src/solution560.rs
@@ -3,6 +3,18 @@ pub struct Solution;
 impl Solution {
     #[allow(dead_code)]
     pub fn subarray_sum(nums: Vec<i32>, k: i32) -> i32 {
+        let mut return_val = 0;
+        
+        for i in 0..nums.len() {
+            let mut current_sum = 0;
+            for j in i..nums.len() {
+                current_sum += nums[j];
+                if current_sum == k {
+                    return_val += 1;
+                }
+            }
+        }
 
+        return_val
     }
 }
