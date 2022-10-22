e16eff0a5fa7bcfec020bece7f92f643531c1ca3
Michael Xu <1378606230@qq.com>
2022-10-22 09:17:05 +0800

    time: 17.74% space: 5.64% for leetcode 121

--- src/solution121.rs
+++ src/solution121.rs
@@ -5,11 +5,10 @@ impl Solution {
     pub fn max_profit(prices: Vec<i32>) -> i32 {
         if prices.len() == 1 { return 0; }
         use std::cmp;
-        let mut max_profit: i32 = 0;
         let mut dp: Vec<i32> = vec![0; prices.len()];
         dp[0] = 0;
         dp[1] = cmp::max(prices[1] - prices[0], 0);
-        max_profit = cmp::max(dp[0], dp[1]);
+        let mut max_profit = cmp::max(dp[0], dp[1]);
         let _tmp = prices.iter().enumerate().map(|(i, c)| {
             if i >= 2 {
                 dp[i] = cmp::max(prices[i] - prices[i - 1] + dp[i - 1], 0);
