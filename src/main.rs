9579aac587c56a97de5f10af1afd212a3ff6a0a1
Michael Xu <1378606230@qq.com>
2022-10-23 11:40:30 +0800

    A little change

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 mod solution;
 mod solution2;
 mod solution5;
-mod solution59;
+mod solution43;
 mod solution48;
 mod solution49;
+mod solution59;
 mod solution119;
-mod solution240;
-mod solution435;
-mod solution290;
-mod solution409;
-mod solution415;
-mod solution763;
-mod solution560;
-mod solution334;
-mod solution392;
-mod solution205;
-mod solution238;
 mod solution121;
 mod solution187;
-mod solution43;
+mod solution205;
+mod solution238;
+mod solution240;
+mod solution290;
+mod solution334;
+mod solution392;
+mod solution409;
+mod solution415;
+mod solution435;
+mod solution560;
+mod solution763;
 
 fn main() {
     println!("hello world");
---
+++ src/solution1480.rs
@@ -0,0 +1,17 @@
+pub struct Solution;
+
+impl Solution {
+	pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+		let mut return_list: Vec<i32> = Vec::new();
+		for (i, elem) in nums.iter().enumerate() {
+			if i == 0 {
+				return_list.push(*elem);
+				continue;
+			} else {
+				return_list.push(*elem + return_list[i - 1]);
+			}
+		}
+
+		return_list
+	}
+}
