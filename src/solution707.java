031aa94a5da4be75fe8600f95d70698e4528ad5d
Michael Xu <1378606230@qq.com>
2022-10-27 14:38:37 +0800

    Change solution707 from Rust to Java

---
+++ src/solution707.java
@@ -0,0 +1,35 @@
+/**
+ * Your MyLinkedList object will be instantiated and called as such:
+ * MyLinkedList obj = new MyLinkedList();
+ * int param_1 = obj.get(index);
+ * obj.addAtHead(val);
+ * obj.addAtTail(val);
+ * obj.addAtIndex(index,val);
+ * obj.deleteAtIndex(index);
+ */
+class MyLinkedList {
+
+    public MyLinkedList() {
+
+    }
+    
+    public int get(int index) {
+
+    }
+    
+    public void addAtHead(int val) {
+
+    }
+    
+    public void addAtTail(int val) {
+
+    }
+    
+    public void addAtIndex(int index, int val) {
+
+    }
+    
+    public void deleteAtIndex(int index) {
+
+    }
+}
