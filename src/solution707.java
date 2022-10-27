2e55d80e35ea75cc98d88138f36c94b874d2bb62
Michael Xu <1378606230@qq.com>
2022-10-27 14:50:20 +0800

    Create basic structure for solution707

--- src/solution707.java
+++ src/solution707.java
@@ -8,13 +8,39 @@
  * obj.deleteAtIndex(index);
  */
 class MyLinkedList {
+    private class Node {    
+        private int val;
+        private Node next;
+        private Node prev;
+
+        public void Node(int val, Node next, Node prev) {
+            val = val;
+            next = next;
+            prev = prev;
+        }
+    }
+
+    private Node firstNode;
+    private Node lastNode;
+    private int size;
 
     public MyLinkedList() {
-
+        firstNode = new Node(0, null, null);
+        lastNode = new Node(0, null, firstNode);
+        firstNode.next = lastNode;
+        size = 0;
     }
     
     public int get(int index) {
-
+        if index >= size {
+            return -1;
+        } else {
+            Node tmpNode = firstNode.next;
+            for (int i = 0; i < index; ++i) {
+                tmpNode = tmpNode.next;
+            }
+            return tmpNode.val;
+        }
     }
     
     public void addAtHead(int val) {
