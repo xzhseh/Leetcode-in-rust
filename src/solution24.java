350642092299522440510e4e59c7bccd48b0b173
Michael Xu <1378606230@qq.com>
2022-10-28 22:22:33 +0800

    Finish the case for the even number of nodes in the singly linkedlist of solution24.java

--- src/solution24.java
+++ src/solution24.java
@@ -13,7 +13,7 @@ class Solution {
         if (head == null || head.next == null) { // Handle some special cases
             return head;
         } else if (head.next.next == null) {
-            ListNode return_node = head.next.next;
+            ListNode return_node = head.next;
             head.next.next = head;
             head.next = null;
             return return_node;
@@ -30,16 +30,27 @@ class Solution {
         while (true) {
             // Exchange the node
             ListNode tmp_node = ptr_one.next.next;
+            ListNode preserve_node = ptr_one.next;
             ptr_one.next.next = ptr_one;
             ptr_one.next = tmp_node;
 
+            if (ptr_one != head) {
+                ListNode Tmp_node = head;
+
+                while (Tmp_node.next != ptr_one) {
+                    Tmp_node = Tmp_node.next;
+                }
+
+                Tmp_node.next = preserve_node;
+            }
+            
             // Judge the end condition of the loop
             if (ptr_one == head) {
                 break;
             }
 
             // Update the ptr_one via the move of ptr_two
-            while (ptr_two.next.next != ptr_one) {
+            while (ptr_two.next.next != preserve_node) {
                 ptr_two = ptr_two.next;
             }
 
