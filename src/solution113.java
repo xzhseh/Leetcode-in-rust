488f041bfa514b6d03b893a5a73bf49cc9ee1cf7
Michael Xu <1378606230@qq.com>
2022-11-02 09:38:29 +0800

    time: 100.00% space: 51.31% for leetcode 113

---
+++ src/solution113.java
@@ -0,0 +1,23 @@
+class Solution {
+    List<List<Integer>> ret = new LinkedList<List<Integer>>();
+    Deque<Integer> path = new LinkedList<Integer>();
+
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        dfs(root, targetSum);
+        return ret;
+    }
+
+    public void dfs(TreeNode root, int targetSum) {
+        if (root == null) {
+            return;
+        }
+        path.offerLast(root.val);
+        targetSum -= root.val;
+        if (root.left == null && root.right == null && targetSum == 0) {
+            ret.add(new LinkedList<Integer>(path));
+        }
+        dfs(root.left, targetSum);
+        dfs(root.right, targetSum);
+        path.pollLast();
+    }
+}
