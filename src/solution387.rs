0115d632551dd0e157520936ba7be366d064d7fd
Michael Xu <1378606230@qq.com>
2022-11-08 21:24:39 +0800

    time: 17.74% space: 8.06% for leetcode 387

--- src/solution387.rs
+++ src/solution387.rs
@@ -1,1 +1,23 @@
 pub struct Solution;
+
+impl Solution {
+    #[allow(dead_code)]
+    pub fn first_uniq_char(s: String) -> i32 {
+		use std::collections::HashMap;
+		let mut return_index: i32 = -1;
+        let mut flag = false;
+		let mut current_hashset: HashMap<char, i32> = HashMap::new();
+		let _tmp = s.chars().map(|x| {
+			current_hashset.entry(x).and_modify(|x| *x += 1).or_insert(1);
+			x
+		}).collect::<Vec<char>>();
+		let _tmp = s.chars().enumerate().map(|(i, c)| {
+			if current_hashset[&c] == 1 && !flag {
+				return_index = i as i32;
+                flag = true;
+			}
+			c
+		}).collect::<Vec<char>>();
+		return_index as i32
+    }
+}
