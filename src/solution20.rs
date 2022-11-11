64ea3f6020d2573eaf3b03c1068eaabe4022f952
Michael Xu <1378606230@qq.com>
2022-11-11 19:47:12 +0800

    time: 100.00% space: 5.39% for leetcode 20

--- src/solution20.rs
+++ src/solution20.rs
@@ -1,2 +1,49 @@
 pub struct Solution;
 
+impl Solution {
+    #[allow(dead_code)]
+    pub fn is_valid(s: String) -> bool {
+		let mut stack: Vec<char> = Vec::new();
+		let mut flag = true;
+		let _tmp = s.chars().map(|x| {
+			match x {
+				')' => {
+					match stack.pop() {
+						Some(val) => {
+							if val != '(' {
+								flag = false;
+							}
+						},
+						None => flag = false,
+					}
+				},
+				']' => {
+					match stack.pop() {
+						Some(val) => {
+							if val != '[' {
+								flag = false;
+							}
+						},
+						None => flag = false,
+					}
+				},
+				'}' => {
+					match stack.pop() {
+						Some(val) => {
+							if val != '{' {
+								flag = false;
+							}
+						},
+						None => flag = false,
+					}
+				},
+				_ => stack.push(x),
+			}
+			x
+		}).collect::<Vec<char>>();
+        if stack.len() != 0 {
+            flag = false;
+        }
+		flag
+    }
+}
