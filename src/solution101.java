6acce426b79abcee88a82b28aa1b567429741aa6
Michael Xu <1378606230@qq.com>
2022-11-13 23:54:06 +0800

    time: 100.00% space: 47.68% for leetcode 101

---
+++ src/solution101.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return helpFunc(root, root);
+    }
+
+    private boolean helpFunc(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        } else if (left == null || right == null) {
+            return false;
+        }
+        return (left.val == right.val) && helpFunc(left.left, right.right) && helpFunc(left.right, right.left);
+    }
+}
