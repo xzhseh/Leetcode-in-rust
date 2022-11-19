8a96122335924a6ccaec0017972ce60800c07b77
Michael Xu <1378606230@qq.com>
2022-11-19 19:12:37 +0800

    time: 70.33% space: 100.00% for leetcode 200

--- src/solution200.rs
+++ src/solution200.rs
@@ -3,11 +3,39 @@ pub struct Solution;
 impl Solution {
     #[allow(dead_code)]
     pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
+        if grid.is_empty() {
+            return 0;
+        }
 
+        let mut return_num = 0;
+        let mut grid = grid.clone();
+
+        for i in 0..grid.len() {
+            for j in 0..grid[0].len() {
+                if grid[i][j] == '1' {
+                    return_num += 1;
+                    Solution::dfs(&mut grid, j as i32, i as i32);
+                }
+            }
+        }
+
+        return_num
     }
 
     #[allow(dead_code)]
-    pub fn dfs(current_grid: &mut Vec<Vec<char>>, col: usize, row: usize) {
-        
+    pub fn dfs(current_grid: &mut Vec<Vec<char>>, col: i32, row: i32) {
+        let grid_row = current_grid.len();
+        let grid_col = current_grid[0].len();
+
+        if col >= grid_col as i32 || row >= grid_row as i32 || col < 0 || row < 0 || current_grid[row as usize][col as usize] == '0' {
+            return;
+        }
+
+        current_grid[row as usize][col as usize] = '0';
+
+        Solution::dfs(current_grid, col + 1, row);
+        Solution::dfs(current_grid, col, row + 1);
+        Solution::dfs(current_grid, col - 1, row);
+        Solution::dfs(current_grid, col, row - 1);
     }
 }
