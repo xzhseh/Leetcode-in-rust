36fea9fc2ae2b2b4b0b6ac4c79312c6b7c3196d9
Michael Xu <1378606230@qq.com>
2022-11-19 19:00:51 +0800

    Optimize the structure of solution200.rs

--- src/solution200.rs
+++ src/solution200.rs
@@ -1,7 +1,13 @@
 pub struct Solution;
 
 impl Solution {
+    #[allow(dead_code)]
     pub fn num_islands(grid: Vec<Vec<char>>) -> i32 {
 
+    }
+
+    #[allow(dead_code)]
+    pub fn dfs(current_grid: &mut Vec<Vec<char>>, col: usize, row: usize) {
+        
     }
 }
