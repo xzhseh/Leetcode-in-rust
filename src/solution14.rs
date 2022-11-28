cf955f7e317b325b9006cdb0c5292591d5f03d83
Michael Xu <1378606230@qq.com>
2022-11-28 11:35:50 +0800

    time: 100.00% space: 72.57% for leetcode 14

--- src/solution14.rs
+++ src/solution14.rs
@@ -3,6 +3,33 @@ pub struct Solution;
 impl Solution {
     #[allow(dead_code)]
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        let mut return_string = String::new();
+        let mut length = strs[0].len();
 
+        for i in 1..strs.len() {
+            if strs[i].len() < length {
+                length = strs[i].len();
+            }
+        }
+
+        for i in 0..length {
+            let mut flag = true;
+            let cur_word = strs[0].chars().nth(i).unwrap();
+
+            for j in 1..strs.len() {
+                if strs[j].chars().nth(i).unwrap() != cur_word {
+                    flag = false;
+                    break;
+                }
+            }
+            
+            if !flag {
+                break;
+            } else {
+                return_string.push(cur_word);
+            }
+        }
+
+        return_string
     }
 }
