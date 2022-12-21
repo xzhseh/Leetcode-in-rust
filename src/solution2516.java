f377f3a71481ea8b4d541a748776b97bfde790f6
Michael Xu <1378606230@qq.com>
2022-12-20 22:22:51 -0500

    time: 56.54% space: 67.65% for leetcode 2516

--- src/solution2516.java
+++ src/solution2516.java
@@ -1,1 +1,38 @@
-d
+class Solution {
+    int ways, n, k;
+    List<List<Integer>> edges;
+
+    public int numWays(int n, int[][] relation, int k) {
+        ways = 0;
+        this.n = n;
+        this.k = k;
+        edges = new ArrayList<List<Integer>>();
+
+        for (int i = 0; i < n; ++i) {
+            edges.add(new ArrayList<Integer>());
+        }
+
+        for (int[] edge : relation) {
+            int src = edge[0];
+            int dst = edge[1];
+            edges.get(src).add(dst);
+        }
+
+        dfs(0, 0);
+
+        return ways;
+    }
+
+    private void dfs(int index, int steps) {
+        if (steps == k) {
+            if (index == n - 1) {
+                ways += 1;
+            }
+            return;
+        }
+        List<Integer> edge = edges.get(index);
+        for (int next_stop : edge) {
+            dfs(next_stop, steps + 1);
+        }
+    }
+}
